0e43d47aa695a09e3f87cce8e3dd370f9e8a6768
Mohamed Ayman <madoo.ayman14@gmail.com>
2018-02-21 22:06:05 +0200

    Propagate registration page change to the User table

--- db/migrate/20180214154947_create_users.rb
+++ db/migrate/20180214154947_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :email
       t.string :password
+      t.string :first_name
+      t.string :last_name
       t.datetime :created_at, default: -> { 'CURRENT_TIMESTAMP' }
     end
   end
